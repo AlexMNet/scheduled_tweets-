52e43f22fc58ef02229bd918d38349c91680727e
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-02-13 11:53:20 -0800

    Add twitter_accounts with omniauth twitter and twitter API

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@
 # password:string virtual
 # password_confirmation: string virtual
 class User < ApplicationRecord
+  has_many :twitter_accounts
   # virtual that creats hashed password and saves that in the database
   has_secure_password
+
   validates :email, presence: true, format: {with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email"}
-  
 end
 
