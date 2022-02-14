4c9ee9d2c948a20199410e86e9b4749a18e6f80e
Alex Maldonado <alex@Alexs-MBP-2021.home>
2022-02-13 23:18:32 -0800

    Add edit and delete tweets. Add form and tweet partials to tweets dir. Add form errors partials.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,17 +1,48 @@
 class TweetsController < ApplicationController
   before_action :require_login!
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
   def index
     @tweets = Current.user.tweets
   end
   
   def new
-    @Tweet = Tweet.new
+    @tweet = Tweet.new
   end
 
-  def show
+  def create
+    @tweet = Current.user.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to tweets_path, notice: "Tweet was scheduled Successfully"
+    else
+      render :new
+    end 
   end
 
-  def destroy
+  def edit 
+    # @tweet set by before_action
+  end
+
+  def update 
+    if @tweet.update(tweet_params)
+      redirect_to tweets_path, notice: "Tweet was updated successfully"
+    else
+      render :new
+    end
+  end
+
+  def destroy 
+    @tweet.destroy
+    redirect_to tweets_path, notice: "Tweet was unscheduled"
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:twitter_account_id, :body, :publish_at)
+  end
+
+  def set_tweet
+    @tweet = Current.user.tweets.find(params[:id])
   end
 end
